10728e2dcef10cc7f13f13c8de797ca264a54e25
OJ Adeyemi <ojieadeyemi@gmail.com>
2025-01-17 18:24:15 -0500

    add nba innovation award

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,47 +11,58 @@ const About = () => {
         <BlurFade delay={BLUR_FADE_DELAY * 5}>
           <div className="flex flex-col gap-y-2">
             <p className="text-sm leading-relaxed text-muted-foreground">
-              Hello! I’m a computer science student based in Toronto. I’m
-              currently an Engineer at MLSE, where I’ve been able to build
-              &nbsp;
-              <a
-                href="https://aws.amazon.com/blogs/media/maple-leaf-sports-entertainment-debuts-generative-ai-video-editor-to-deliver-content-to-fans-faster/"
-                className="inline underline transition duration-150 ease-in-out hover:text-purple-300 active:text-purple-600"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Gen AI applications
-              </a>
-              &nbsp; for the Toronto Raptors, Maple Leafs, TFC, and Argonauts.
+              Hello! I’m a computer science student based in Toronto and
+              currently an Engineer at MLSE, where I help build &nbsp;
+              <strong>
+                <a
+                  href="https://aws.amazon.com/blogs/media/maple-leaf-sports-entertainment-debuts-generative-ai-video-editor-to-deliver-content-to-fans-faster/"
+                  className="inline underline transition duration-150 ease-in-out hover:text-purple-300 active:text-purple-600"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Gen AI applications
+                </a>
+              </strong>
+              &nbsp; for the Toronto Raptors, Maple Leafs, TFC, Argonauts and
+              other MLSE teams.
             </p>
             <p className="text-sm leading-relaxed text-muted-foreground">
-              One of these applications has been &nbsp;
-              <a
-                href="https://www.sports-business-awards-tech.com/2025/2025-nominees#:~:text=MLSE%20Digital%20Labs%E2%80%99%20Generative%20Video%20Editor"
-                className="inline underline transition duration-150 ease-in-out hover:text-purple-300 active:text-purple-600"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                nominated for the prestigious Sports Business Awards: Tech Best
-                in AI category
-              </a>
-              &nbsp; by Sports Business Journal, recognizing significant
-              advancements in sports technology. It also has the potential to
-              win the coveted title of{" "}
-              <strong>2025 Sports Technology of the Year</strong>.
+              As part of the team that won the{" "}
+              <strong>
+                <a
+                  href="https://www.nba.com/news/nba-announces-sales-marketing-awards-2025#:~:text=NBA%20Team%20Innovation%20Award%3A%20Toronto%20Raptors"
+                  className="inline underline transition duration-150 ease-in-out hover:text-purple-300 active:text-purple-600"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  2025 NBA Team Innovation Award
+                </a>
+              </strong>
+              , I've contributed to AI-driven advancements in the sports
+              industry. One of these applications has also been &nbsp;
+              <strong>
+                <a
+                  href="https://www.sports-business-awards-tech.com/2025/2025-nominees#:~:text=MLSE%20Digital%20Labs%E2%80%99%20Generative%20Video%20Editor"
+                  className="inline underline transition duration-150 ease-in-out hover:text-purple-300 active:text-purple-600"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  nominated for the prestigious Sports Business Awards: Tech
+                  Best in AI,
+                </a>
+              </strong>
+              &nbsp; by Sports Business Journal, with the potential to win 2025
+              Sports Technology of the Year.
             </p>
 
             <p className="text-sm leading-relaxed text-muted-foreground">
               Outside of work, I enjoy team sports 🏀 ⚽ 🏈 and music ˚♬ ﾟ.
               These hobbies keep me active, motivated, and creatively inspired.
-            </p>
-
-            <p className="text-sm leading-relaxed text-muted-foreground">
               I’m passionate about developing my computer science skills, with a
               focus on continuous innovation and advancing the sports technology
-              industry. My goal is to solve complex problems and design scalable
-              software architectures that are efficient, reliable, and built for
-              the future.
+              industry. I’m also passionate about making technology more
+              accessible, fostering curiosity, and empowering others to engage
+              with and shape its future.
             </p>
           </div>
         </BlurFade>
