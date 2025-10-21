4c39c7b2b129e568d5033ab7d8e40c90cbb0360a
OJ Adeyemi <ojieadeyemi@gmail.com>
2025-10-21 16:54:59 -0400

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,11 +37,20 @@ export const metadata: Metadata = {
     description: "OJ Adeyemi's portfolio.",
     url: "https://ojadeyemi.github.io/",
     type: "website",
+    images: [
+      {
+        url: "/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "OJ Adeyemi's Portfolio",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "OJ Adeyemi",
     description: "OJ Adeyemi's portfolio.",
+    images: ["/og_image.png"],
   },
   robots: "index, follow",
   alternates: {
